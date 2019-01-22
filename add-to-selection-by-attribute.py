885c0ab2d8136cb295c64c36af1ee05c1b818e1f
Audrey <audreylmassmann@gmail.com>
2019-01-22 16:08:11 -0500

    Update add-to-selection-by-attribute.py
    option to remove

--- add-to-selection-by-attribute.py
+++ add-to-selection-by-attribute.py
@@ -3,5 +3,5 @@
 # Import system modules
 import arcpy
 
-# Include trunks in primary road
+# Include trunks in primary road, use REMOVE_FROM_SELECTION to remove
 arcpy.SelectLayerByAttribute_management("clipped_roads_openstreetmap_primary", "ADD_TO_SELECTION", "fclass = 'trunk'", "NON_INVERT")
